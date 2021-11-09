b7ab0c31ec3bb0b22230c9e3fc9de1c70d5edbe5
richie-south <richard.soderman@gmail.com>
2021-11-09 14:16:27 +0100

    chore: lib files

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,13 +1,10 @@
 export declare type Match<T = unknown> = {
     match: string;
+    index: number;
     type: T;
     [key: string]: any;
 };
 export interface Parser<T = unknown> {
-    findMatch: (text: string) => {
-        value: string;
-        index: number;
-    } | null;
-    converter: (match: string) => Match<T>;
+    parse: (text: string) => Match<T> | null;
 }
 export declare type MatchResults<T = unknown> = Array<Match<T> | string>;
